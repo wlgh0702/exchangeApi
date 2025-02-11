a0e97a38f8094511581417a16bc8bf7521247252
jiho <parkjiho723@gmail.com>
2025-02-11 22:41:57 +0900

    init commit

---
+++ exchangeApi/src/test/java/com/gamjacoding/exchangeapi/ExchangeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamjacoding.exchangeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
